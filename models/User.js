8da04b8bcc6cd14b528553cf1b4a5463642c4725
Ethan Sroka <ethansroka@gmail.com>
2024-02-07 02:02:10 -0700

    reaction model created. Other models updated.

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+\@.+\..+/, 'Invalid email address.'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return `${this.friends.length}`;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
